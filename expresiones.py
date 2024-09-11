8fe5d67ee68664bc114961d30388aa6b5e7ff02e
Luis Eduardo Perez Osorio <lued2897@gmail.com>
2024-09-11 14:14:04 -0600

    correccion ejercicio 7

--- expresiones.py
+++ expresiones.py
@@ -20,7 +20,7 @@ ejercicios[4] = re.compile('^(?![a-z]*[aeiou]{3})[a-z]*$')
 #ejercicio6
 ejercicios[5] = re.compile('(?=[a-z]*[aiueo])(?=[a-z]*[bcdfghjklmnpqrstvwxyz][a-z]*[bcdfghjklmnpqrstvwxyz][a-z]*)[a-z]*|[bcdfghjklmnpqrstvwxyz]*')
 #ejercicio7
-ejercicios[6] = re.compile('([013579](\d){2,}[13579]|[02468](\d){2,}[02468])')
+ejercicios[6] = re.compile('([13579](\d){2,}[13579]|[02468](\d){2,}[02468])')
 
 
 #para obtener las coincidencias: re.match(string), regresa un arreglo con las coincidencias
