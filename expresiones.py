7bef962e9b5aff48df700583597f4370c4abf244
Luis Eduardo Perez Osorio <lued2897@gmail.com>
2024-09-11 20:02:45 -0600

    salida de texto

--- expresiones.py
+++ expresiones.py
@@ -12,7 +12,7 @@ ejercicios[0] = re.compile('^[0-9]([5-9][0-9])*$')
 #ejercicio2
 ejercicios[1] = re.compile('^([13579][0-9])*[13579]?$')
 #ejercicio
-ejercicios[2] = re.compile('((a[ab])|(a[ab]a)|(a[ab]a[ab])|(a[ab]a[ab]a))|((a[ab]a[ab]aa)+(|a|(a[ab])|(a[ab]a)|(a[ab]a[ab])|(a[ab]a[ab]a)))') #'^(?=(?:a[ab])*a?$)(?=(?:[ab]{2}a)*[ab]{0,2}$)[ab]+$'
+ejercicios[2] = re.compile('((a[ab])|(a[ab]a)|(a[ab]a[ab])|(a[ab]a[ab]a))|((a[ab]a[ab]aa)+(|a|(a[ab])|(a[ab]a)|(a[ab]a[ab])|(a[ab]a[ab]a)))')
 #ejercicio4
 ejercicios[3] = re.compile('^[aeiou]([a-z]{2}|[a-z]*[aeiou][a-z]{2})')
 #ejercicio5
